904ae4d2ca731baa35eff634f99b07900a45cadc
TszyaoEkaterina <3022805283@qq.com>
2022-12-25 02:04:42 -0500

    Ex2 max words added

--- src/Comparator.java
+++ src/Comparator.java
@@ -1,13 +1,24 @@
 public class Comparator implements java.util.Comparator<Person> {
+    int maxWords;
 
     @Override
     public int compare(Person o1, Person o2) {
-        if (o1.getSurname().split(" ").length > o2.getSurname().split(" ").length) {
+        int wordsCount1 = o1.getSurname().split(" ").length;
+        int wordsCount2 = o2.getSurname().split(" ").length;
+        if (wordsCount1 >= maxWords && wordsCount2 >= maxWords) {
+            return Integer.compare(o1.getAge(), o2.getAge());
+        }
+        if (wordsCount1 > wordsCount2) {
             return 1;
-        } else if (o1.getSurname().split(" ").length < o2.getSurname().split(" ").length) {
+        } else if (wordsCount1 < wordsCount2) {
             return -1;
         } else {
             return Integer.compare(o1.getAge(), o2.getAge());
+
         }
+    }
+
+    public Comparator(int maxWords) {
+        this.maxWords = maxWords;
     }
 }
